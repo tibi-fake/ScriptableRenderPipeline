baa3dc0271b82317e78130cbec1eec8daf408149
Frédéric Vauchelles <fredericv@unity3d.com>
2018-10-26 16:51:56 +0200

    Added test template

---
+++ com.unity.render-pipelines.high-definition/Tests/Editor/SampleEditorTests.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.TestTools;
+using UnityEditor;
+
+class SampleEditorTests
+{
+    [Test]
+    public void DummyTest()
+    {
+        Assert.IsTrue(true);
+    }
+}
