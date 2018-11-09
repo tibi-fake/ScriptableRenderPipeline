0de95d32e10a3e5b73be142e79ebc016a781737e
Tibor Hencz <tibor.hencz@fake.fi>
2018-11-09 15:11:38 +0200

    Fix Vector1 property serialization on cultures where decimal separator is other than a dot

--- com.unity.shadergraph/Editor/Data/Graphs/Vector1ShaderProperty.cs
+++ com.unity.shadergraph/Editor/Data/Graphs/Vector1ShaderProperty.cs
@@ -74,7 +74,7 @@ namespace UnityEditor.ShaderGraph
             {
                 case FloatType.Slider:
                     result.Append("\", Range(");
-                    result.Append(m_RangeValues.x + ", " + m_RangeValues.y);
+                    result.Append(String.Format("{0:0.0###}, {1:0.0###}", m_RangeValues.x, m_RangeValues.y));
                     result.Append(")) = ");
                     break;
                 case FloatType.Integer:
